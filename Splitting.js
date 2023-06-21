749151772cfddb8e569a333d4b77d27d6a433dfc
Simon Kye <kkeysimon@gmail.com>
2023-06-21 14:48:58 +0800

    shows table after pressing process

--- Splitting.js
+++ Splitting.js
@@ -1,40 +1,33 @@
 import React, { useState } from 'react';
-import Table from "react-native-table-component";
+import { Table, Row, Rows } from "react-native-table-component";
+import { StyleSheet, View } from 'react-native';
 
 export default function Splitting(imageData) {
     console.log(imageData['imageData'])
-    const [data, setData] = useState(imageData['imageData']);
-    console.log(data)
-    const columns = [
-        {
-            title: "Item Name",
-            dataKey: "ITEM",
-        },
-        {
-            title: "Price",
-            dataKey: "PRICE",
-        }
-    ];
-    console.log(typeof data)
-    const styles = {
-        table: {
-            borderRadius: 10,
-            borderColor: "#ccc",
-            borderWidth: 1,
-        },
-        row: {
-            padding: 10,
-        },
-        cell: {
-            fontSize: 16,
-        },
-    };
+    const tableData = imageData['imageData'];
+    
 
     return (
-        <Table
-            data={data}
-            columns={columns}
-            styles={styles}
-        />
+        <View style={styles.container}>
+            <Table borderStyle={styles.table}>
+                <Row
+                    data={Object.keys(tableData[0])} // Use the keys of the first object as table headers
+                    style={styles.head}
+                    textStyle={styles.headText}
+                />
+                <Rows
+                    data={tableData.map((rowData) => Object.values(rowData))}
+                    textStyle={styles.rowText}
+                />
+            </Table>
+        </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
+    table: { borderWidth: 1, borderColor: '#c1c0b9' },
+    head: { height: 40, backgroundColor: '#f1f8ff' },
+    headText: { margin: 6, fontWeight: 'bold' },
+    rowText: { margin: 6 },
+});
