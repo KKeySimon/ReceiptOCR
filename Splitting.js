bb7e80acc42ac124820c1bee39e0cf8578b31065
rishabvivek <rishabv752@gmail.com>
2023-06-23 15:41:46 -0700

    base ui done need to figure out scrolling for when list gets long and also change back button logic

--- Splitting.js
+++ Splitting.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import { Table, Row, Rows } from "react-native-table-component";
 import { ScrollView, StyleSheet, View, Dimensions } from 'react-native';
 import { ButtonGroup, ListItem } from '@rneui/base';
 import { Button, Text } from '@rneui/themed';
+
 
 export default function Splitting(imageData) {
     const tableData = imageData['imageData'];
@@ -33,113 +34,132 @@ export default function Splitting(imageData) {
     <ScrollView>
         {!dataConfirmed ?
         <View style = {styles.container}>
-            
-            <View style = {{ marginTop: 150, marginBottom: -150}}>
-                <Text h1 style = {{textAlign: 'center', fontWeight: 'bold', color: '#02c736'}}>Looks Good?</Text>
-                <Text style = {{textAlign: 'center', marginTop: 30, marginBottom: -30}}> Check if all the information is correct.</Text>
-            </View>
 
-            <View style = {styles.listItemContainer}>
-                {tableData.map((rowData, index) => (
-                    <ListItem bottomDivider key={index}>
-                        <ListItem.Content style = {styles.listItemContent}>
-                            <ListItem.Title style = {{color: "#02c736", fontWeight: 'bold'}}>{rowData['ITEM']}</ListItem.Title>
-                            <ListItem.Subtitle>Price: ${rowData['PRICE']}</ListItem.Subtitle>
-                        </ListItem.Content>
-                    </ListItem>
-                ))}
-            </View>
-            <View style = {styles.buttonContainer}>
-                <Button color="#02c736" buttonStyle = {{borderRadius: 10}} size="md" onPress={() => {setDataConfirmed(true); calcTotalPrice()}}>Continue</Button>
-            </View>
-        </View>
-        :
-        <View>
-            <Text h2 align = "center" containerStyle={{ marginBottom: 20 }}>Total Price: {totalPrice}</Text>
-            
-            <Text h4 align = "center" containerStyle={{ marginBottom: 20 }}> Number of People</Text>
-            <Button size="md" onPress={() => {if (numPeople != 0) {
-                setNumPeople(numPeople - 1)
-            }}}>-</Button>
-            <Text h4> {numPeople}</Text>
-            <Button size="md" onPress={() => setNumPeople(numPeople + 1)}>+</Button>
+                <View style = {{ marginTop: 150, marginBottom: -150}}>
+                    <Text h1 style = {{textAlign: 'center', fontWeight: 'bold', color: '#02c736'}}>Looks Good?</Text>
+                    <Text style = {{textAlign: 'center', marginTop: 30, marginBottom: -30}}> Check if all the information is correct.</Text>
+                </View>
 
-            <Text align = "center" style = {styles.subHeader}
-                containerStyle={{ marginBottom: 20 }}
-            >Choose How To Split</Text>
-            <ButtonGroup
-                align = "center"
-                buttons= {["EVENLY", "INDIVIDUALLY"]}
-                selectedIndex = {evenOrIndiv}
-                onPress = {(value) => {
-                    setEvenOrIndiv(value);
-                }}
-                containerStyle={{ marginBottom: 20 }} />
-            {evenOrIndiv !== 0 ?
-                <View style = {styles.container} align = "center">
-                    <Text>Select Which User Ordered Which Dish</Text>
-                    <ButtonGroup 
-                        buttons={ Array.from({length: numPeople}, (_, i) => i + 1)}
-                        selectedIndex={selectedUser}
-                        onPress={(value => 
-                            {
-                                setSelectedUser(value);
-                                for (let i = 0; i < usersItems.length; i++) {
-                                    if (usersItems[i][value] === true) {
-                                        checked[i] = true;
-                                    } else {
-                                        checked[i] = false;
-                                    }
-                                }
-                            }
-                        )}
-                    />
+
+
+                <View style = {styles.listItemContainer}>
                     {tableData.map((rowData, index) => (
-                        <ListItem 
-                            key={index}
-                        bottomDivider>
-                            <ListItem.CheckBox
-                            // Use ThemeProvider to change the defaults of the checkbox
-                            iconType="material-community"
-                            checkedIcon="checkbox-marked"
-                            uncheckedIcon="checkbox-blank-outline"
-                            checked={checked[index]}
-                            onPress={() => setChecked((prevList) => {
-                                    const newList = [...prevList];
-                                    newList[index] = !newList[index];
-                                    return newList;
-                                })
-                            }
-                            />
-                            <ListItem.Content>
-                                <ListItem.Title>{rowData['ITEM']}</ListItem.Title>
-                                <ListItem.Subtitle>{rowData['PRICE']}</ListItem.Subtitle>
+                        <ListItem bottomDivider key={index}>
+                            <ListItem.Content style = {styles.listItemContent}>
+                                <ListItem.Title style = {{color: "#02c736", fontWeight: 'bold'}}>{rowData['ITEM']}</ListItem.Title>
+                                <ListItem.Subtitle>Price: ${rowData['PRICE']}</ListItem.Subtitle>
                             </ListItem.Content>
-
-                            {usersItems[index].map((item, index) => {
-                                if (item) {
-                                    return <Text key={index}>{index + 1}</Text>
-                                }
-                            })}
-                            <ListItem.Chevron />
                         </ListItem>
                     ))}
-                <Button align="center" onPress={() => 
-                    checked.forEach((item, index) => {if (item) {
-                        const updatedItems = [...usersItems];
-                        updatedItems[index][selectedUser] = true;
-                        setUsersItems(updatedItems);
-                    } else {
-                        const updatedItems = [...usersItems];
-                        updatedItems[index][selectedUser] = false;
-                        setUsersItems(updatedItems);
-                    }})
-                }>Add</Button> 
                 </View>
-                : <View></View>
-            }
-        
-            <Button align = "center">CONTINUE</Button> 
+           
+                <View style = {styles.buttonContainer}>
+                    <Button color="#02c736" size="md" onPress={() => {setDataConfirmed(true); calcTotalPrice()}}>Continue</Button>
+                </View>
+        </View>
+        : 
+        <View style = {styles.container}>
+
+            <View style = {{marginTop: 125, marginBottom:0}}>
+                <Text h2 style = {{textAlign: 'center',  marginBottom: 20}} >Total Price:</Text>
+                <Text h1 style = {{textAlign: 'center', fontWeight: 'bold', color: '#02c736'}} containerStyle = {{marginBottom: 20}}>{totalPrice}</Text>
+                <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 20, marginTop:30 }}>
+                    <Text h4 align = "center" containerStyle={{ marginBottom: 20 }}> Number of People:</Text>
+                    <View style = {{marginLeft: 20 , flexDirection: 'row', alignItems: 'center'}}>
+                        <Button buttonStyle = {{marginRight: 30}} color = '#fff' titleStyle = {{color:'#02c736'}} size="md" onPress={() => {if (numPeople != 0) {
+                            setNumPeople(numPeople - 1)
+                        }}}>-</Button>
+                        <Text h4 style = {{fontWeight: 'bold'}}> {numPeople}</Text>
+                        <Button buttonStyle = {{marginLeft: 30}} color = '#fff' titleStyle = {{color:'#02c736'}} size="md" onPress={() => setNumPeople(numPeople + 1)}>+</Button>
+                    </View>
+                </View>
+                <Text style = {{textAlign: 'center'}}
+                    containerStyle={{ marginBottom: 20 }}
+                >Choose How To Split</Text>
+                <ButtonGroup
+                    align = "center"
+                    buttons= {["EVENLY", "INDIVIDUALLY"]}
+                    selectedIndex = {evenOrIndiv}
+                    onPress = {(value) => {
+                        setEvenOrIndiv(value);
+                    }}
+                    containerStyle={{ marginBottom: 20}}
+                    selectedButtonStyle = {{backgroundColor: '#02c736'}}
+                    selectedTextStyle = {{color: 'white'}}
+
+                     />
+                </View>    
+                <View style = {styles.listItemContainer}>
+                {evenOrIndiv !== 0 ?
+                    <View style = {styles.container} align = "center">
+                        <Text>Select Which User Ordered Which Dish</Text>
+                        <ButtonGroup
+                            selectedButtonStyle = {{backgroundColor: '#02c736'}}
+                            selectedTextStyle = {{color: 'white'}}
+                            buttons={ Array.from({length: numPeople}, (_, i) => i + 1)}
+                            selectedIndex={selectedUser}
+                            onPress={(value => 
+                                {
+                                    setSelectedUser(value);
+                                    for (let i = 0; i < usersItems.length; i++) {
+                                        if (usersItems[i][value] === true) {
+                                            checked[i] = true;
+                                        } else {
+                                            checked[i] = false;
+                                        }
+                                    }
+                                }
+                            )}
+                        />
+                        {tableData.map((rowData, index) => (
+                            <ListItem 
+                                key={index}
+                            bottomDivider>
+                                <ListItem.CheckBox
+                                // Use ThemeProvider to change the defaults of the checkbox
+                                checkedColor='#02c736'
+                                iconType="material-community"
+                                checkedIcon="checkbox-marked"
+                                uncheckedIcon="checkbox-blank-outline"
+                                checked={checked[index]}
+                                onPress={() => setChecked((prevList) => {
+                                        const newList = [...prevList];
+                                        newList[index] = !newList[index];
+                                        return newList;
+                                    })
+                                }
+                                />
+                                <ListItem.Content>
+                                    <ListItem.Title style = {{color: "#02c736", fontWeight: 'bold'}}>{rowData['ITEM']}</ListItem.Title>
+                                    <ListItem.Subtitle>${rowData['PRICE']}</ListItem.Subtitle>
+                                </ListItem.Content>
+
+                                {usersItems[index].map((item, index) => {
+                                    if (item) {
+                                        return <Text key={index}>{index + 1}</Text>
+                                    }
+                                })}
+                            </ListItem>
+                        ))}
+                    <Button align="center" size = 'md' color="#02c736" buttonStyle = {{borderRadius: 10}} style = {{width: '100%', justifyContent: 'center', alignItems: 'center', marginTop: 20}} onPress={() => 
+                        checked.forEach((item, index) => {if (item) {
+                            const updatedItems = [...usersItems];
+                            updatedItems[index][selectedUser] = true;
+                            setUsersItems(updatedItems);
+                        } else {
+                            const updatedItems = [...usersItems];
+                            updatedItems[index][selectedUser] = false;
+                            setUsersItems(updatedItems);
+                        }})
+                    }>Add Items</Button> 
+                    </View>
+                    : <View></View>
+                }
+            </View>
+            
+            <View style = {styles.buttonContainer}>
+                <Button color="#02c736"  size="md">CONTINUE</Button> 
+            </View>
 
             
         </View>
@@ -164,6 +184,7 @@ export default function Splitting(imageData) {
 
 const styles = StyleSheet.create({
     container: { flex: 1, backgroundColor: '#fff', width: Dimensions.get('window').width, height: Dimensions.get('window').height},
+
     listItemContainer: {
         flex: 1,
         justifyContent: 'center',
@@ -173,11 +194,10 @@ const styles = StyleSheet.create({
         alignItems: 'center',
       },
     buttonContainer: {
-        position: 'absolute',
-        bottom: 20,
+        marginTop: 50,
         width: '100%',
-        padding: 20,
-        justifyContent: 'center',
-        alignItems: 'center',
+
       },
+    
 });
+
